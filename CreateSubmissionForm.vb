9abc4c254b74a993a07350c9c8484c43153a7e88
akshitha-108 <139312740+akshitha-108@users.noreply.github.com>
2024-06-21 20:22:33 +0530

    Add files via upload

---
+++ CreateSubmissionForm.vb
@@ -0,0 +1,31 @@
+﻿Public Class CreateSubmissionForm
+    Private stopwatch As New Stopwatch()
+
+    Private Sub btnStartPause_Click(sender As Object, e As EventArgs) Handles btnStartPause.Click
+        If stopwatch.IsRunning Then
+            stopwatch.Stop()
+            btnStartPause.Text = "Resume"
+        Else
+            stopwatch.Start()
+            btnStartPause.Text = "Pause"
+        End If
+    End Sub
+
+    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+        ' Code to submit data to the backend server
+        Dim submission As New Dictionary(Of String, String) From {
+            {"name", txtName.Text},
+            {"email", txtEmail.Text},
+            {"phone", txtPhone.Text},
+            {"github_link", txtGithub.Text},
+            {"stopwatch_time", stopwatch.Elapsed.ToString()}
+        }
+        ' Call the backend API to submit the data
+    End Sub
+
+    Private Sub CreateSubmissionForm_KeyDown(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+        If e.Control AndAlso e.KeyCode = Keys.S Then
+            btnSubmit.PerformClick()
+        End If
+    End Sub
+End Class
---
+++ ViewSubmissionsForm.vb
@@ -0,0 +1,31 @@
+﻿Public Class ViewSubmissionsForm
+    Private currentIndex As Integer = 0
+    Private submissions As List(Of Dictionary(Of String, String))
+
+    Private Sub ViewSubmissionsForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Load the first submission
+        LoadSubmission(currentIndex)
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        currentIndex += 1
+        If currentIndex < submissions.Count Then
+            LoadSubmission(currentIndex)
+        Else
+            currentIndex -= 1
+        End If
+    End Sub
+
+    Private Sub btnPrevious_Click(sender As Object, e As EventArgs) Handles btnPrevious.Click
+        currentIndex -= 1
+        If currentIndex >= 0 Then
+            LoadSubmission(currentIndex)
+        Else
+            currentIndex += 1
+        End If
+    End Sub
+
+    Private Sub LoadSubmission(index As Integer)
+        ' Code to load the submission data from the backend server
+    End Sub
+End Class
